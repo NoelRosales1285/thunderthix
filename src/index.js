c117a0174664c5ad078bbca1be6b4616bfc7ff5b
NoelRosales1285 <48063303+NoelRosales1285@users.noreply.github.com>
2019-05-29 16:12:58 -0500

    noel
    asasasas

--- src/index.js
+++ src/index.js
@@ -1,76 +1,81 @@
-import axios from "axios";
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
+import InfiniteScroll from "react-infinite-scroll-component";
+import axios from "axios";
+import Grid from "@material-ui/core/Grid";
+
+const style = {
+  height: 80,
+  width: 200,
+  border: "3px solid green",
+  margin: 6,
+  padding: 8
+};
+
+const baseUrl = "https://demo.thunderstage.com/api/public_events";
+
+const getUsers = async page => {
+  try {
+    return await axios.get(baseUrl + "?page=" + page);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   state = {
-    posts: [],
-    isLoading: true,
-    errors: null
+    items: Array.from({ length: 20 }),
+    page: 1,
+    res: [],
+    hasMore: true
+  };
+  componentWillMount() {
+    getUsers(this.state.page).then(res => {
+      this.fetchMoreData();
+    });
+  }
+
+  fetchMoreData = () => {
+    console.log("feching...page:", this.state.page);
+    getUsers(this.state.page).then(res => {
+      this.setState({
+        res: this.state.res.concat(Array.from(res.data.objects)),
+        page: this.state.page + 1,
+        hasMore: res.data.total_pages > this.state.page
+      });
+    });
   };
 
-  getPosts() {
-    axios
-      .get("https://admin.thunderstage.com/barcode/events.json", {
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-          "Access-Control-Allow-Origin": "*",
-          Authorization: "Basic  ZGVtbzk6ZGVtb2RlbW8="
-        }
-      })
-      .then(response => {
-        this.setState({
-          posts: response.data,
-          isLoading: false
-        });
-      })
-      .catch(error => {
-        console.log(error.status);
-        console.log(error.message);
-        console.log(error.stack);
-        console.log(error.config);
-      });
-  }
-
-  componentDidMount() {
-    this.getPosts();
-  }
-
   render() {
-    const { isLoading, posts } = this.state;
     return (
-      <React.Fragment>
-        <h2>Events</h2>
-        <div>
-          {!isLoading ? (
-            posts.map(post => {
-              const { id, name, seating_chart } = post;
-              return (
-                <div key={id}>
-                  <p>
-                    <strong>User ID:</strong> {id}
-                  </p>
-                  <p>
-                    <strong>Name:</strong> {name}
-                  </p>
-                  <p>
-                    <strong>Seating Chart:</strong> {seating_chart}
-                  </p>
-                  <hr />
+      <div>
+        <h1>demo: react-infinite-scroll-component</h1>
+        <hr />
+        <InfiniteScroll
+          dataLength={this.state.res.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.hasMore}
+          loader={<h4>Loading...</h4>}
+        >
+          <Grid container spacing={40}>
+            {this.state.res.map((i, index) => (
+              <Grid key={index * 10} item>
+                <div style={style} key={index}>
+                  <mark>{i.id}</mark> <br />
+                  {i.name}
+                  <br />
+                  {i.seating_chart_id}
                 </div>
-              );
-            })
-          ) : (
-            <div id="loading">
-              <img src="_loading.gif" alt="loading" />
-              <p>loading...</p>
-            </div>
-          )}
-        </div>
-      </React.Fragment>
+              </Grid>
+            ))}
+          </Grid>
+        </InfiniteScroll>
+      </div>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById("root"));
